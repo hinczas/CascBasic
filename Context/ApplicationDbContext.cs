86d2e1f0243a519458804c40e389d9ea10d32dce
hinczas <hinczas@gmail.com>
2019-03-05 18:13:49 +0000

    init

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using CascBasic.Models;
+
+namespace CascBasic.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
